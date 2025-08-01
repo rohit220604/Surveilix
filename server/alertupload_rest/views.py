e08a28e50d2ea374178e6e39cfd9f6bc61f2e431
rohit220604 <rjrohit2264@gmail.com>
2025-08-02 01:02:43 +0530

    Bug solved

--- server/alertupload_rest/views.py
+++ server/alertupload_rest/views.py
@@ -5,12 +5,11 @@ from django.http import JsonResponse
 from threading import Thread
 from django.core.mail import send_mail
 import re
-from twilio.rest import Client
 from django.conf import settings
 
 def start_new_thread(function):
     def decorator(*args, **kwargs):
-        t = Thread(target = function, args=args, kwargs=kwargs)
+        t = Thread(target=function, args=args, kwargs=kwargs)
         t.daemon = True
         t.start()
     return decorator
@@ -18,44 +17,45 @@ def start_new_thread(function):
 @api_view(['POST'])
 def post_alert(request):
     serializer = UploadAlertSerializer(data=request.data)
-
     if serializer.is_valid():
         serializer.save()
-        identify_email_sms(serializer)
+        identify_email(data=serializer.data)
+        return JsonResponse({'success': True})
     else:
-        return JsonResponse({'error':'Unable to process data'},status=400)
-    
-    return Response(request.META.get('HTTP_AUTHORIZATION'))
+        return JsonResponse({'error': 'Unable to process data'}, status=400)
 
-def identify_email_sms(serializer):
-    if(re.search(r'^[^@]+@[^@]+\.[^@]+$', serializer.data['alert_receiver'])):  
+def identify_email(data):
+    alert_receiver = data.get('alert_receiver', '')
+    # Regex pattern for email only
+    email_pattern = re.compile(r'^[^@]+@[^@]+\.[^@]+$')
+
+    if email_pattern.match(alert_receiver):
         print("Valid Email")
-        send_email(serializer)
-    elif re.compile(r'^\d{10}$').match(serializer.data['alert_receiver']):
-        print("Valid Mobile Number")
-        send_sms(serializer)
+        send_email(data)
     else:
-        print("Invalid Email or Mobile number")
+        print("Invalid Email")
 
 @start_new_thread
-def send_email(serializer):
-    send_mail('Weapon Detected!', 
-    prepare_alert_message(serializer), 
-    'rjrohit2264@gmail.com',
-    [serializer.data['alert_receiver']],
-    fail_silently=True,)
+def send_email(data):
+    send_mail(
+        'Weapon Detected!',
+        prepare_alert_message(data),
+        'rjrohit2264@gmail.com',
+        [data['alert_receiver']],
+        fail_silently=True,
+    )
 
-@start_new_thread
-def send_sms(serializer):
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-
-    message = client.messages.create(body=prepare_alert_message(serializer),from_=settings.TWILIO_NUMBER,to=serializer.data['alert_receiver'])
-
-def prepare_alert_message(serializer):
-    image_data = split(serializer.data['image'], ".")
-    uuid = split(image_data[3],'/')
-    url = 'http://127.0.0.1:8000/alert' + uuid[2]
-
+def prepare_alert_message(data):
+    image_val = data.get('image', '')
+    image_data = split(image_val, ".")
+    if len(image_data) > 3:
+        uuid = split(image_data[3], '/')
+        if len(uuid) > 2:
+            url = 'http://127.0.0.1:8000/alert' + uuid[2]
+        else:
+            url = 'http://127.0.0.1:8000/alert'
+    else:
+        url = 'http://127.0.0.1:8000/alert'
     return 'Weapon Detected! View alert at ' + url
 
 def split(value, key):
