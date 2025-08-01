0ae55b70dd3dd76ae147685b238ec893b4a0b93c
rohit220604 <rjrohit2264@gmail.com>
2025-08-02 01:55:07 +0530

    added final host

--- server/wd_ss/settings.py
+++ server/wd_ss/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://surveilix.onrender.com']
+ALLOWED_HOSTS = ['surveilix.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
